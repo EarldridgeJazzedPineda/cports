62337bb98ef23f2f2902476dfdf9f1ca16c0e8e9
Earldridge Jazzed Pineda <earldridgejazzedpineda@gmail.com>
2024-08-31 10:37:02 +0800

    user/xapp-apps: new package

---
+++ user/xapp-apps/template.py
@@ -0,0 +1,17 @@
+pkgname = "xapp-apps"
+pkgver = "1"
+pkgrel = 0
+build_style = "meta"
+depends = [
+    # "blueberry", # requires an older version of gnome-bluetooth
+    "bulky",
+    "pix",
+    # "timeshift", # FIXME: timeshift-gtk causes segmentation fault at strlen()
+    "xdg-desktop-portal-xapp",
+    "xed",
+    "xreader",
+    "xviewer",
+]
+pkgdesc = "Collection of X-Apps applications"
+license = "custom:meta"
+url = "https://projects.linuxmint.com/xapps"
